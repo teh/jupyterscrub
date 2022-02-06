b2d7b66e1b7057168cecd3c9fffef3bd1981a207
Tom Hunger <tehunger@gmail.com>
2022-02-06 22:10:06 +0000

    Better error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
+use anyhow::Context;
+
 use clap::{arg, App};
 
 mod nbformat;
 mod scrub;
 
-
 fn scrub_one(path: &str) -> anyhow::Result<()> {
-    let input = std::fs::read_to_string(path)?;
-    let result = scrub::scrub(&input)?;
+    let input =
+        std::fs::read_to_string(path).with_context(|| format!("Can't read file {}", path))?;
+    let result = scrub::scrub(&input).with_context(|| format!("Can't process file {}", path))?;
 
     if result.modified {
-        std::fs::write(path,result.json)?;
+        std::fs::write(path, result.json).with_context(|| format!("Can't write file {}", path))?;
     }
 
     Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
-    let matches = App::new("jupyterscrub").args(&[
-        arg!(<input> ... "input files"),
-    ]).get_matches();
+    let matches = App::new("jupyterscrub")
+        .args(&[arg!(<input> ... "input files")])
+        .get_matches();
 
-    matches.values_of("input").unwrap().try_for_each(scrub_one)?;
+    matches
+        .values_of("input")
+        .unwrap()
+        .try_for_each(scrub_one)?;
 
     Ok(())
 }
